95b88d5a5a7481ada518c501c250d3bef3fbb7aa
syedrummanabbas <syedrummanabbas@gmail.com>
2026-02-05 23:21:25 +0530

    Add files via upload

---
+++ PythonProject1/task1.py
@@ -0,0 +1,16 @@
+#Problem Statement: Write a Python program that does the following:
+#1.  Takes two numbers as input from the user.
+#2.  Performs the basic mathematical operations on these two numbers:
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
